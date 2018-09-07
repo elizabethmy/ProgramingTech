9b3fca202ebd029816d9db4b62f4a094ec3d016b
Lê Thị Diễm My <lethidiemmy961996@gmail.com>
2018-09-07 17:45:31 +0700

    * choose type to test Pointer

--- daynhauhoc/daynhauhoc_function/Pointer.cpp
+++ daynhauhoc/daynhauhoc_function/Pointer.cpp
@@ -2,11 +2,44 @@
 
 using namespace std;
 
+enum pointer
+{
+POINTER_TYPE,
+POINTER_
+};
+
 int main() {
-	float* fPtr;
-	float fVal = 20;
-	fPtr = &fVal;
-	cout <<"type of &fVal: "<< typeid(&fVal).name() << endl;
+	pointer test = (pointer)0;
+
+	cout << "Choose your test: " << endl;
+	cout << "============================================================================================\n" << endl;
+	//cin >> test;
+	test = POINTER_TYPE;
+
+	switch (test)
+	{
+		case POINTER_TYPE: 
+		{
+			cout << "\n============= POINTER_TYPE ===============" << endl;
+			float* fPtr;
+			float fVal = 20;
+			fPtr = &fVal;
+			cout << "type of &fVal: " << typeid(&fVal).name() << endl;
+			break;
+		} 
+		case POINTER_:
+		{
+			cout << "\n============= POINTER_TYPE ===============" << endl;
+			float* fPtr;
+			float fVal = 20;
+			fPtr = &fVal;
+			cout << "type of &fVal: " << typeid(&fVal).name() << endl;
+			break;
+		}			
+		default:
+			break;
+	}
+
 	system("pause");
 	return 0;
 }
