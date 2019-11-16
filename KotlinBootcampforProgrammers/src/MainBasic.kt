4d2cfeb8551275fb8eee73fe097019eea56d2be1
MyLeWhiteShark <lethidiemmy961996@gmail.com>
2019-11-16 19:09:46 +0700

    finish Kotlin basic and function

--- KotlinBootcampforProgrammers/src/MainBasic.kt
+++ KotlinBootcampforProgrammers/src/MainBasic.kt
@@ -1,4 +1,4 @@
-fun main(args: Array<String>) {
+fun main() { //args: Array<String>
     var c: Int //variable
     val d: Int
     var a: Int = -1
@@ -58,12 +58,37 @@ fun main(args: Array<String>) {
     println("...")
     println("number")
     longNumberReadable()
+
+    //class
+    var cat = Cat()
+    with(cat) {
+        run()
+        print("\n Are you hungry?")
+        walk()
+        print("\n Common! I'll feed you ^^")
+        eat()
+        print("\n You're full. Come and play with me!")
+        lazy()
+    }
+
+    //swap 2 variables
+    var aVar = 1
+    var bVar = 2
+
+    bVar = a.also { aVar = bVar }
+
+    //first lamda function
+    firstLamdaFunc()
 }
 
 fun sum(a: Int, b: Int): Int {
     if (a < 0 || b < 0) {
         return 0
-    } else if (b == 1 && a == 1) {
+    } else if (b == 1 &&
+        a == 1 ||
+        a == 9 &&
+        b == 9
+    ) {
         return 2
     }
     return a + b
@@ -71,14 +96,14 @@ fun sum(a: Int, b: Int): Int {
 
 fun minus(a: Int, b: Int) = a - b
 
-fun voidFun(): Unit //or can omit Unit
+fun voidFun(): Unit // or can omit Unit
 {
     println("No thing! Just a void function")
 }
 
 fun max(a: Int, b: Int) = if (a > b) a else b
 
-fun getStringLength(obj: Any): Int? //must return null
+fun getStringLength(obj: Any): Int? // must return null
 {
     if (obj is String) {
         return obj.length
@@ -153,6 +178,31 @@ fun printListExample(): Unit {
     val listM = mutableListOf<String>("One", "Two", "Three", "Four", "Five")
     listM.remove("Four")
     println("\n $listM")
+
+    val listNew = listM.filter { it[0] == 'T' }
+    val sequenceNewNotHolding = listM.asSequence().filter { it[0] == 'T' }
+    println("\n $listNew")
+    println("\n Create a new sequence $sequenceNewNotHolding")
+    val listNewNoHolding = sequenceNewNotHolding.toList()
+    println("\n Convert to a new list $listNewNoHolding")
+
+    //this is a map
+    val lazyMap = listM.asSequence().map {
+        println("access: $it")
+        it
+    }
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
+
+    //you can't change what array/list refer to, but you can change the content of the list/array
+    //you can nest array and list
+    val intArr = intArrayOf(1, 2, 3)
+    val listContainArr = listOf(intArr, "happy", "value") //list with variety content
+    println("\n $listContainArr")
+
 }
 
 fun longNumberReadable(): Unit {
@@ -172,3 +222,51 @@ fun checkNull(): Unit {
     checkNull?.equals("world") ?: false
     checkNull.compareTo("ahihi") //throw any Exception if value is null
 }
+
+abstract class Animal {
+    abstract fun run()
+    abstract fun walk()
+    abstract fun eat()
+    abstract fun sleep()
+}
+
+class Cat : Animal() {
+    override fun run() {
+        println("\n I'm running")
+    }
+
+    override fun walk() {
+        println("\n I'm walking")
+    }
+
+    override fun eat() {
+        println("\n I'm eating")
+    }
+
+    override fun sleep() {
+        println("\n I'm sleeping")
+    }
+
+    fun lazy() {
+        println("\n I'm lazy")
+    }
+}
+
+fun firstLamdaFunc() {
+    var meaningLife = "What is the meaning of the life?"
+    val findMeaning = { -> println("Live the life fullest for each moment") }
+
+    val findMeaning2: (String) -> String = { str: String ->
+        println("Live the life fullest for each moment")
+        str
+    }
+    updateFirstLamdaFunc(meaningLife, findMeaning2)
+
+    updateFirstLamdaFunc(meaningLife,::increaseDirty)
+}
+
+fun updateFirstLamdaFunc(strVar: String, operation: (String) -> String): String {
+    return operation(strVar)
+}
+
+fun increaseDirty( start: String ) = "start + 1"
