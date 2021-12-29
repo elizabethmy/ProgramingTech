4568e954b6e39bb4b703c22fa085aef02bb6f54f
Le Thi Diem My <lethidiemmy961996@gmail.com>
2021-12-29 21:26:32 +0700

    Day4: Loop Advance

--- c++/nvns/Loop/LoopAdvance.cpp
+++ c++/nvns/Loop/LoopAdvance.cpp
@@ -4,7 +4,28 @@
 * Customer: give money
 * Saleman: exchange money
 */
+enum CurrencDenomination //VND
+{
+	FIVE_HUNDRED_THOUSAND = 500000,
+	TWO_HUNDRED_THOUSAND = 200000,
+	ONE_HUNDRED_THOUSAND = 100000,
+	FIFTY_THOUSAND = 50000,
+	TWENTY_THOUSAND = 20000,
+	TEN_THOUSAND = 10000,
+	FIVE_THOUSAND = 5000,
+	TWO_THOUSAND = 2000,
+	ONE_THOUSAND = 1000,
+	FIVE_HUNDRED = 500
+};
+
 int main()
 {
+	//input
+	float price = 0.f, customerPaid = 0.f;
+	int quantity = 0;
+
+	//output
+
+
 	return 0;
 }
