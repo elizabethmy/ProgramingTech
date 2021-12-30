4487ef0d25721a157d8e0b07f5c1996d4551a804
Le Thi Diem My <lethidiemmy961996@gmail.com>
2021-12-30 20:36:43 +0700

    Day 5: Exercise Loop Advance

--- c++/nvns/Loop/LoopAdvance.cpp
+++ c++/nvns/Loop/LoopAdvance.cpp
@@ -22,10 +22,78 @@ int main()
 {
 	//input
 	float price = 0.f, customerPaid = 0.f;
-	int quantity = 0;
+	float exchange = 0.f;
+	//Amount to be paid by user
+	do
+	{
+		std::cout << "Please input the amount user must pay: ";
+		std::cin >> price;
+		std::cout << "" << std::endl;
+		std::cout << "Please input the money user give: ";
+		std::cin >> customerPaid;
+	} while (price < 0.f || (customerPaid - price) < 0.f);
 
 	//output
-
+	float fiveHundreadThousandNote = 0.f;
+	float twoHundreadThousandNote = 0.f;
+	float oneHundreadThousandNote = 0.f;
+	float fiftyThousandNote = 0.f;
+	float twentyThousandNote = 0.f;
+	float tenThousandNote = 0.f;
+	float fiveThousandNote = 0.f;
+	float twoThousandNote = 0.f;
+	float oneThousandNote = 0.f;
+	float fiveHundreadNote = 0.f;
+	std::cout << "The amount the customer has to change: " << std::endl;
+	exchange = customerPaid - price;
+	
+	if(exchange == 0)
+	{
+		std::cout << "Does not need to give user the exchange. " << std::endl;
+	}
+	else
+	{
+		if(exchange >= FIVE_HUNDRED_THOUSAND)
+		{
+			
+		}
+		else if (exchange >= TWO_HUNDRED_THOUSAND)
+		{
+			
+		}
+		else if (exchange >= ONE_HUNDRED_THOUSAND)
+		{
+			
+		}
+		else if (exchange >= FIFTY_THOUSAND)
+		{
+			
+		}
+		else if (exchange >= TWENTY_THOUSAND)
+		{
+			
+		}
+		else if (exchange >= TEN_THOUSAND)
+		{
+			
+		}
+		else if(exchange >= FIVE_THOUSAND)
+		{
+			
+		}
+		else if (exchange >= TWO_THOUSAND)
+		{
+			
+		}
+		else if (exchange >= ONE_THOUSAND)
+		{
+			
+		}
+		else if(exchange >= FIVE_HUNDRED)
+		{
+			
+		}
+	}
 
 	return 0;
 }
