d3031b7ee6b06a3ac719950e441500b90890eb64
Le Thi Diem My <lethidiemmy961996@gmail.com>
2022-01-11 11:30:23 +0700

    (Day14) Rename folder courses

---
+++ c++/vietnamson/Function/FunctionExercisePart1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+/*
+Bài 1: Viết chương trình trò chơi oẳn tù tì giữa người và máy theo thể lệ người luôn đi trước.
+
+Luật chơi như sau: Người nhập vào ký tự tương ứng:
+'B' hay 'b' => Búa
+'O' hay 'o' => Bao
+'K' hay 'k' => kéo
+
+Sau đó Máy sẽ random ra ký tự bất kỳ (có thể là búa hoặc bao hoặc kéo). Sau đó tính kết quả chung cuộc (người thắng hay máy thắng).
+
+Gợi ý: Tham khảo cách Random số nguyên trong 1 đoạn nào đó, ta có thể quy ước vd: số 1 => búa, 2 => bao, 3 => kéo. Rồi tính thắng thua giữa người & máy theo nguyên tắc như ngoài đời:
+
+- Búa thắng kéo nhưng thua bao
+- Bao thắng búa nhưng thua kéo
+- Kéo thắng bao nhưng thua búa.
+*/
+void ScissorHammerBag()
+{
+}
+
+int main()
+{
+	return 0;
+}
